5f2e32b0b08bf4607335edc0295af7e98f6e008a
Andresflores17 <142111724+Andresflores17@users.noreply.github.com>
2023-08-26 15:31:20 -0600

    Add files via upload

---
+++ Practica1/script.js
@@ -0,0 +1,16 @@
+var miPrimerVariable ='hola, mundo'
+
+var campoTexto = document.getElementById('mitexto1')
+
+
+function mostrarAlerta(){
+    alert(campoTexto.value);
+}
+
+function limpiarTexto(){
+
+    document.getElementById("mitexto1").value = "";
+
+}
+
+   
