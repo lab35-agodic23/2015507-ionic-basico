2f2185536158e568714071ba14eeb36f83bd310b
Andresflores17 <142111724+Andresflores17@users.noreply.github.com>
2023-09-02 14:14:16 -0600

    Add files via upload

---
+++ Practica 4/index.js
@@ -0,0 +1,12 @@
+var gasto= [];
+
+var gasto=document.getElementById("gasto");
+var gasto=document.getElementById("monto");
+
+
+function Mostrarvalores(){
+
+const Ngast=`${gasto.value} $${monto.value}`;
+gastos.push(Ngasto);
+console.log(gasto);
+}
